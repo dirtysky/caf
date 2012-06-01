45d4bc2929ed62e60111143519503345b302b52e
Salman Qazi <sqazi@google.com>
2012-05-31 23:51:27 -0400

    ext4: add ext4_mb_unload_buddy in the error path
    commit 02b7831019ea4e7994968c84b5826fa8b248ffc8 upstream.
    
    ext4_free_blocks fails to pair an ext4_mb_load_buddy with a matching
    ext4_mb_unload_buddy when it fails a memory allocation.
    
    Signed-off-by: Salman Qazi <sqazi@google.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -4639,6 +4639,11 @@ do_more:
 		 */
 		new_entry = kmem_cache_alloc(ext4_free_data_cachep,
 				GFP_NOFS|__GFP_NOFAIL);
+		if (!new_entry) {
+			ext4_mb_unload_buddy(&e4b);
+			err = -ENOMEM;
+			goto error_return;
+		}
 		new_entry->efd_start_cluster = bit;
 		new_entry->efd_group = block_group;
 		new_entry->efd_count = count_clusters;
